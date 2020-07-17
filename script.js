9f55558639c039bcc203e26eb744ae77307f8f5e
justinwilliamsrva <justin.williams2@me.com>
2020-07-17 17:25:43 -0400

    Random index in 2  boxes

---
+++ script.js
@@ -0,0 +1,7 @@
+var cases = ["Dog", "Evil", "Cow"];
+
+var ex = cases[Math.floor(Math.random() * 3)];
+var ex1 = cases[Math.floor(Math.random() * 3)];
+
+document.getElementById("box1_1").innerHTML = ex;
+document.getElementById("box1_2").innerHTML = ex1;
